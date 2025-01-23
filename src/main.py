b3a6e44bd15050128254e00b8e3a882b19a51ccb
JizhouHuang <jackson.z.hamilton@gmail.com>
2025-01-23 03:44:56 -0600

    delete unused classes

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ import pandas as pd
 from tqdm import tqdm
 from tabulate import tabulate
 from .experiments.experiment_ccsc import ExperimentCCSC
-from .utils.data import UCIMedicalDataset
 
 def main(data_name: str):
 
